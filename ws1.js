19e56786d4dfe5f02f0c2da52ef9238983cef7e0
Hojat Fathpour <tsunamy66>
2022-12-15 11:27:43 +0330

    prettier

--- ws1.js
+++ ws1.js
@@ -1,16 +1,16 @@
-var http = require('http')
-var fs = require('fs');
-var {WebSocketServer} = require('ws');
+var http = require("http");
+var fs = require("fs");
+var { WebSocketServer } = require("ws");
 
-const server = http.createServer()
-const wss = new WebSocketServer({ noServer : true });
+const server = http.createServer();
+const wss = new WebSocketServer({ noServer: true });
 
-wss.on('connection', function connection(ws) {
-  ws.on('message', function message(data) {
-    console.log('received: %s', data);
+wss.on("connection", function connection(ws) {
+  ws.on("message", function message(data) {
+    console.log("received: %s", data);
   });
 
-  ws.send('something');
+  ws.send("something");
 });
 
 server.listen(8080);

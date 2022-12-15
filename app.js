0578146f0125f1a4bc0771d1098c2dc2d7bf3082
tsunamyh <fathpour.hojat@gmail.com>
2022-12-16 00:43:21 +0330

    commit for push

--- app.js
+++ app.js
@@ -1,23 +1,22 @@
-const http = require('http')
-const path = require('path')
-const express = require('express')
-const app = express()
+const http = require("http");
+const path = require("path");
+const express = require("express");
+const app = express();
 
-app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, 'views')) 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 app.set("PORT", process.env.PORT || 8080);
 
-// app.use(express.static(path.join(__dirname,'views')));
-app.use(express.static(path.join(__dirname, 'views')))
+app.use(express.static(path.join(__dirname, "views")));
 
-app.get('/',function (req,res,next) {
+app.get("/", function (req, res, next) {
   // res.send('<h1>'+ 'testing' + '</h1>')
-  res.sendFile("index.html", { root: path.join(__dirname, 'views') })
-})
+  res.sendFile("index.html"); //, { root: path.join(__dirname, 'views') })
+});
 
-const server = http.createServer(app)
-server.listen(app.get('PORT'),()=>{
-  console.log(`server is listening on port ${app.get('PORT')}`);
-})
+const server = http.createServer(app);
+server.listen(app.get("PORT"), () => {
+  console.log(`server is listening on port ${app.get("PORT")}`);
+});
 
-module.exports = server
+module.exports = server;

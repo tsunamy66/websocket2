440579ea496484d4e4d04a650defefb59ccb394f
Hojat Fathpour <tsunamy66>
2022-12-15 12:14:39 +0330

    require app

--- ws.js
+++ ws.js
@@ -1,20 +1,22 @@
-const { WebSocketServer } = require('ws');
+const { WebSocketServer } = require("ws");
+const server = require("./app");
 
-var wss = new WebSocketServer({ port: 3000 });
+var wss = new WebSocketServer({ server });
 
-wss.on('connection', function connection(ws) {
-    console.log(typeof(ws))
-    ws.on('message', function message(message) {
-        if (message.toString() === 'exit') {
-            ws.close();
-        } else {
-          wss.clients.forEach(function (client) {
-              client.send(message.toString())
-          })
-        }
-        console.log(message.toString());
-    });
-    ws.send('Hello World!!');
+wss.on("connection", function connection(ws) {
+  console.log(typeof ws);
+  ws.on("message", function message(message) {
+    if (message.toString() === "exit") {
+      ws.close();
+    } else {
+      console.log("client::", wss.clients.size);
+      wss.clients.forEach(function (client) {
+        client.send(message.toString());
+      });
+    }
+    console.log("SERVER SIDE::", message.toString());
+  });
+  ws.send("Hello World!!");
 });
 
-console.log('websocket server is running on port 3000');
+console.log("websocket server is running ");

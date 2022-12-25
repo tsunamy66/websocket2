28760df47a26f69fc52593f90335a4ed8a57b439
tsunamyh <fathpour.hojat@gmail.com>
2022-12-25 23:53:24 +0330

    handle error 404 in express

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
+const errorHandler = require("http-errors")
 const path = require("path");
 const express = require("express");
 const session = require('express-session');
 
-const userRouter = require("./routes/3userController");
+const userRouter = require("./routes/4userRouter");
 
 const app = express();
 
@@ -11,16 +12,32 @@ app.set("view engine", "ejs");
 app.set("PORT", process.env.PORT || 8080);
 
 app.use(express.json())
-app.use(express.static(path.join(__dirname, "views")));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
 
-app.use('/',userRouter)
+app.use('/user', userRouter)
 
-app.get("/", function (req, res, next) {
+app.get("/home", function (req, res, next) {
   res.render("home");
 });
 
 app.get("/chat", function (req, res, next) {
   res.render("chat")
 });
+
+app.use((req, res, next) => {
+  next(errorHandler(404))
+})
+
+app.use((err, req, res, next) => {
+  console.log(err)
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // console.log("req.app.get('env')=>",req.app.get('env')); //development
+  // console.log('process.env.NODE_ENV=>',process.env.NODE_ENV);
+  res.status(err.status || 500);
+  res.render('error');
+})
+
 
 module.exports = app;
